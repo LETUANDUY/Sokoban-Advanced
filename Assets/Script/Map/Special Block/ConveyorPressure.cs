fa7b0523d9f42adb6dcf2625f201ff74389eec1c
LETUANDUY <duylckt033@gmail.com>
2025-12-02 19:53:00 +0700

    Add Electric Box, Power Tile, Teleporter, Conveyor

---
+++ Assets/Script/Map/Special Block/ConveyorPressure.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using ObserverPattern;
+
+public class ConveyorPressure : MonoBehaviour, IPowerRequire
+{
+    
+    [SerializeField] private Diraction diraction;
+    [Range(0, 3)]
+    [SerializeField] private int NavigatorID;
+    [SerializeField] private bool isPowered = true;
+    
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private BoxCollider2D boxCollider2D;
+    [SerializeField] private GameObject EnergyRing;
+
+    private bool originalPowerState;
+
+
+    private void Awake()
+    {
+        originalPowerState = isPowered;
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //trì hoãn Gửi tín hiệu thay đổi hướng
+        Invoke(nameof(ChangeDiraction), 0.1f);
+            
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Observer.PostEvent(EvenID.ChangeDiraction, new object[] {NavigatorID, null});
+    }
+
+    //Gửi tín hiệu thay đổi hướng
+    private void ChangeDiraction()
+    {
+        Observer.PostEvent(EvenID.ChangeDiraction, new object[] {NavigatorID, diraction});
+    }
+
+    public void ResetLevel()
+    {
+        SetPowerState(originalPowerState);
+    }
+
+
+    /// Thiết lập trạng thái nguồn
+    public void SetPowerState(bool state)
+    {
+        isPowered = state;
+        boxCollider2D.enabled = isPowered;
+        EnergyRing.SetActive(isPowered);
+
+        // Khi tắt nguồn, đảm bảo gửi tín hiệu dừng đổi hướng
+        if (!isPowered)
+        {
+            Observer.PostEvent(EvenID.ChangeDiraction, new object[] {NavigatorID, null});
+        }
+    }
+
+    #if UNITY_EDITOR
+    private void OnValidate()
+    {
+        ColorChanger.ChangeColor(spriteRenderer, NavigatorID);
+        boxCollider2D.enabled = isPowered;
+        EnergyRing.SetActive(isPowered);
+    }
+    #endif
+  
+}
